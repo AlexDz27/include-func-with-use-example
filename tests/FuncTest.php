4fbac6c3c7c57fdea76a0253d2ac55f7c3647279
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-06-21 19:49:25 +0300

    work

--- tests/FuncTest.php
+++ tests/FuncTest.php
@@ -1,9 +1,18 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use function func\getDifferenceInList;
 
 final class FuncTest extends TestCase {
-  public function testResults() {
-    $this->assertEquals();
+  public function testResults1() {
+    $this->assertEquals(2, getDifferenceInList('1', 'a'));
+  }
+
+  public function testResults2() {
+    $this->assertEquals(4, getDifferenceInList('11', 'aa'));
+  }
+
+  public function testResults3() {
+    $this->assertEquals(1, getDifferenceInList('11', '1ab'));
   }
 }
